4b77305d91b46c931cc3bebda9861ee94d6aef2d
Taylor Daniska <taylordaniska@gmail.com>
2016-02-24 18:04:51 -0500

    Update speedometer_gps.ino

--- speedometer_gps.ino
+++ speedometer_gps.ino
@@ -10,10 +10,8 @@ static void print_float(float val, float invalid, int len, int prec);
 static void print_int(unsigned long val, unsigned long invalid, int len);
 static void print_date(TinyGPS &gps);
 static void print_str(const char *str, int len);
-
 const int stepsPerRevolution = 200; 
 const int buttonPin = 2;
-
 
 Stepper myStepper(stepsPerRevolution, 5,6,8,7);            
 
@@ -27,115 +25,73 @@ int stepValue = 0;
 void setup()
 {
   Serial.begin(9600);
-pinMode(buttonPin, INPUT); 
+  pinMode(buttonPin, INPUT); 
   ss.begin(9600);
 }
 
 void loop()
 {
-  
-  
-  
-  
-  
-  
-
-    
-   unsigned long start = millis();
+  unsigned long start = millis();
   do 
   {
     while (ss.available())
-      gps.encode(ss.read());
+    gps.encode(ss.read());
   } while (millis() - start < 10);          //change 10 to adjust delay for gps
-
-
-
-
-
-
-
-if (startup == true) {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-     for (int i=0; i <= 30; i++) {
-        myStepper.step(-1);
-        delay(10);
-      }
+  if (startup == true) {
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH) {
+  for (int i=0; i <= 30; i++) {
+  myStepper.step(-1);
+  delay(10);
+  }
   }
   Serial.print("Startup");
   for (int i=0; i <= 68; i++) {
-    Serial.print("for");
-    myStepper.step(1);
-    delay(10);
-    buttonState = digitalRead(buttonPin);
-    if (buttonState == HIGH) {     
-     startup = false;
-      for (int i=0; i <= 44; i++) {
-        Serial.print("return");
-        myStepper.step(-1);
-        delay(10);
-      }
-    break;  
-    } 
+  Serial.print("for");
+  myStepper.step(1);
+  delay(10);
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {     
+  startup = false;
+  for (int i=0; i <= 44; i++) {
+  Serial.print("return");
+  myStepper.step(-1);
+  delay(10);
+  }
+  break;  
+  } 
   }
   if(startup == true) {
-    for (int i=0; i <= 68; i++) {
-    Serial.print("for");
-    myStepper.step(-1);
-    delay(10);
-    buttonState = digitalRead(buttonPin);
-    if (buttonState == HIGH) {     
-     startup = false;
-      for (int i=0; i <= 53; i++) {
-        Serial.print("return");
-        myStepper.step(-1);
-        delay(10);
-      }
-    break;  
-    } 
-  }
-  
-  
-  }
-  
-  
-  
-  
-  startup = false;
-}
-
-  Serial.print("Run");
-  
-  //int sensorReading = analogRead(A0);
-  
-  speedValue = (gps.f_speed_kmph()*0.621371);
-  //speedValue = 60;
-  
-  
-  
-  
+  for (int i=0; i <= 68; i++) {
+  Serial.print("for");
+  myStepper.step(-1);
   delay(10);
-
-  //int speedValue = map(sensorReading, 0, 1023, 0, 80);
- 
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {     
+  startup = false;
+  for (int i=0; i <= 53; i++) {
+  Serial.print("return");
+  myStepper.step(-1);
+  delay(10);
+  }
+  break;  
+  } 
+  }
+  }
+  startup = false;
+  }
+  Serial.print("Run");
+  speedValue = (gps.f_speed_kmph()*0.621371);
+  delay(10);
   stepValue = map(speedValue, 0, 80, 0, 63);
-  
   if (stepValue > currentStep) {
-    myStepper.step(1);
-    currentStep = currentStep + 1;
+  myStepper.step(1);
+  currentStep = currentStep + 1;
   }
-  
   else if (stepValue < currentStep) {
-    myStepper.step(-1);
-    currentStep = currentStep - 1;
-    
+  myStepper.step(-1);
+  currentStep = currentStep - 1;
   }
-  
-
-
-
-      
-  
 }
 
 
